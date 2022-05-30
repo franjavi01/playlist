8e57c2c02c9241cdf6f6cb744d6a7495e30a4bd7
franjavi01 <fjsg0001@gmail.com>
2022-05-30 15:08:12 +0200

    añadiendo estilos de bootstrap al formulario

--- index.php
+++ index.php
@@ -4,36 +4,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Mi lista de reprodución</title>
 </head>
 <body>
 
-<?php
-include "conexionBaseDatos.php";
-$conexion = mysqli_connect("localhost", "root", "", "playlist");
-$sql = "SELECT * FROM mis canciones favoritas";
-$query = mysqli_query($conexion, $sql);
-?>
-
-<form action="#" method="post">
-        <div class="mb-3">
-            <label for="cancion" class="form-label">Nombre de la canción:</label>
-            <input type="text" class="form-control" name="cancion">
+<?php include "conexionBaseDatos.php"; ?>
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+            <form action="#" method="post">
+                <div class="mb-3">
+                    <label for="cancion" class="form-label">Nombre de la canción:</label>
+                    <input type="text" class="form-control" name="cancion">
+                </div>
+                <div class="mb-3">
+                    <label for="artista" class="form-label" >Artista:</label>
+                    <input type="text" class="form-control" name="artista">
+                </div>
+                <div class="mb-3">
+                    <label for="año" class="form-label-file">Año:</label>
+                    <input type="number" min="1900" max ="2100" class="form-control" name="año">
+                </div>
+                <div class="mb-3">
+                    <label for="url" class="form-label">Enlace:</label>
+                    <input type="text" class="form-control" name="url">
+                </div>
+                <button type="submit" class="btn btn-warning" name="guardar">Enviar</button>
+            </form>
         </div>
-        <div class="mb-3">
-            <label for="artista" class="form-label" >Artista:</label>
-            <input type="text" class="form-control-file" name="artista">
-        </div>
-        <div class="mb-3">
-            <label for="año" class="form-label">Año:</label>
-            <input type="number" class="form-control" name="año">
-        </div>
-        <div class="mb-3">
-            <label for="url" class="form-label">Enlace:</label>
-            <input type="text" class="form-control" name="url">
-        </div>
-        <button type="submit" class="btn btn-warning" name="guardar">Enviar</button>
-    </form>
 
     <!-- <table class="table table-hover">
         <thead>
@@ -45,6 +45,9 @@ $query = mysqli_query($conexion, $sql);
                 <th scope="col">Borrar</th>
             </tr>
         </thead> -->
+    </div>    
+</div>    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <tbody>
 </body>
 </html>
